ca2f47e3f155c6c686d4a9c487fe6bee5af5f884
olukowade samson <solukowade@gmail.com>
2024-10-23 13:38:24 +0100

    added new lines to package.json and viteconfig.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/React-practice-confrence-event-planner",
+  base: "/React-practice-confrence-event-planner-",
   plugins: [react()],
 })
